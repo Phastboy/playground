20c7947ee343803bbbabdbbff38c3f8f54846a9f
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2024-01-27 02:28:44 +0100

    drawing bar/boxes with c

---
+++ c/bars.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int bars=5, i,j;
+    for (i = 0; i < bars; i++) {
+        for (j = 0;j < bars;j++) {
+            printf("#");
+        }
+        printf("\n");
+    }
+    return 0;
+}
---
+++ c/type.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int x, y;
+
+    x=7;
+    y=5;
+
+    printf("if x=%d y=%d\n", x, y);
+    printf("x/y equals %d\n", x/y);
+    printf("(float) x/y  equals %f\n", (float)x/y);
+    return 0;
+}
