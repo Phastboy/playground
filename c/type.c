72351211cfe824eeb5472e6c4363ee46423ccb85
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2024-02-17 12:09:04 +0100

    type casting

--- c/type.c
+++ c/type.c
@@ -11,9 +11,10 @@ int main(void)
 {
     int x, y;
 
-    x=7;
-    y=5;
-
+    printf("enter your quotent: ");
+    scanf("%d", &x);
+    printf("enter your divisor: ");
+    scanf("%d", &y);
     printf("if x=%d y=%d\n", x, y);
     printf("x/y equals %d\n", x/y);
     printf("(float) x/y  equals %f\n", (float)x/y);
